73aa36572af578b586f69aba54fc7d128b838a84
Joshlreid <m265244@usna.edu>
2023-10-12 09:47:04 -0400

    fish game 4life

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
-print("les go")
+import pygame
+import time
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("Chomp!")
+
+screen.fill((155, 0, 255))
+pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+pygame.draw.rect(screen, (0,170,0), (300, 75, 75, 75))
+
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            print("no today! this games runs 4eva")
