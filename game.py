2424d99ce053a31406a8e7d752157f808b45533a
Joshlreid <m265244@usna.edu>
2023-10-18 09:27:34 -0400

    use parametric varibales

--- game.py
+++ game.py
@@ -1,20 +1,32 @@
 import pygame
-import time
+import sys
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64
+
+# COLORS
+WATER_COLOR = (114, 159, 232)
+SAND_COLOR = (100, 25, 0)
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
 
-screen.fill((155, 0, 255))
-pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
-pygame.draw.rect(screen, (0,170,0), (300, 75, 75, 75))
-
-
-pygame.display.flip()
+screen.fill(WATER_COLOR)
+pygame.draw.rect(screen, SAND_COLOR,
+                 (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand, (SCREEN_WIDTH / 2 - TILE_SIZE / 2, SCREEN_HEIGHT / 2 - TILE_SIZE / 2))
 
 while True:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            print("no today! this games runs 4eva")
+            print("thank fo play")
+            pygame.quit()
+            sys.exit()
+
+        pygame.display.flip()
