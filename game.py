aa5a10993cf2d17a82cad86682b9f58d84fc6f96
Joshlreid <m265244@usna.edu>
2023-10-19 08:53:06 -0400

    game font and seagrass

--- game.py
+++ game.py
@@ -1,25 +1,41 @@
 import pygame
 import sys
+import random
 
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 400
-SAND_HEIGHT = 20
-TILE_SIZE = 64
-
-# COLORS
-WATER_COLOR = (114, 159, 232)
-SAND_COLOR = (100, 25, 0)
+from settings import *
 
 pygame.init()
 
+# fonts
+game_font = pygame.font.Font("assets/fonts/Black_Crayon.ttf", 80)
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-
 screen.fill(WATER_COLOR)
 pygame.draw.rect(screen, SAND_COLOR,
                  (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (SCREEN_WIDTH / 2 - TILE_SIZE / 2, SCREEN_HEIGHT / 2 - TILE_SIZE / 2))
+sand_top = pygame.image.load("assets/images/sand_top.png").convert()
+seagrass = pygame.image.load("assets/images/seagrass.png").convert()
+
+# blit sand tiles across the bottom of the screen
+sand_top.set_colorkey((0, 0, 0))
+for i in range(SCREEN_WIDTH//TILE_SIZE):
+    screen.blit(sand, (TILE_SIZE*i, SCREEN_HEIGHT - TILE_SIZE))
+    screen.blit(sand_top, (TILE_SIZE*i, (SCREEN_HEIGHT - (2*TILE_SIZE))))
+
+# randomly place 4 pieces of grass along the bottom of the screen
+seagrass.set_colorkey((0,0,0))
+for _ in range(4):
+    x = random.randint(0, SCREEN_WIDTH)
+    y = random.randint(SCREEN_HEIGHT - 2*TILE_SIZE, SCREEN_HEIGHT) - (0.5 * TILE_SIZE)
+    screen.blit(seagrass, (x,y))
+
+# drawing font
+text = game_font.render("Chomp!", True, (255, 69, 0))
+screen.blit(text, (SCREEN_WIDTH//2 - text.get_width()//2,
+                   SCREEN_HEIGHT//2 - text.get_height()//2))
+
 
 while True:
     for event in pygame.event.get():
